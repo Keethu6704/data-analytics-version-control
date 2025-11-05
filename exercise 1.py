a4c264d02c352b81eefe5f6bad76e1b1b336f63a
Keethu6704 <keerthanaravi67@gmail.com>
2025-11-05 13:36:17 +0530

    python folder uploaded

---
+++ exercise 1.py
@@ -0,0 +1,15 @@
+#a=input("enter the value of a=")
+#b=input("enter the value of b=")
+#temp=a
+#a=b
+#b=temp
+#print("a=" + a)
+#print("b=" + b)
+
+a = 5
+print(id(a))
+
+a = 8
+print(id(a))
+
+print(a is a)
